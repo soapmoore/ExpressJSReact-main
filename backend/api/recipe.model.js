2ea46d7b5081ab386146c616d50d24f302e0d94b
Sophia Moore <sms1390@nyu.edu>
2021-11-30 18:43:47 -0500

    initial commit by sophia

---
+++ backend/api/recipe.model.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+// const Schema = mongoose.Schema;
+
+const RecipeSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  image: String,
+  year: String,
+});
+
+const Recipe = mongoose.model("Recipe", RecipeSchema);
