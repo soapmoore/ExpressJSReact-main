ac377d8af278f52279edc7af4427abe28eb5f943
Sophia Moore <sms1390@nyu.edu>
2021-11-30 20:54:38 -0500

    this is a fail, custom hook

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+import Recipes from "./Recipes";
+import RecipeDetail from "./RecipeDetail";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Toggle from "./Toggle";
+
+function App() {
+  const [recipes, setRecipes] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch(`/api/recipes`)
+      .then((response) => response.json())
+      .then((data) => setRecipes(data));
+  }, []);
+
+  return (
+    <main>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Recipes recipes={recipes} />} />
+          <Route
+            path="/:recipeId"
+            element={<RecipeDetail recipes={recipes} />}
+          />
+        </Routes>
+      </BrowserRouter>
+    </main>
+  );
+}
+
+export default App;
